c4eebd9b8dd523474dc1a3e46dfcefdea0b51859
TimMorrisDev <timmorris_16@hotmail.com>
2021-07-07 20:06:43 +0000

    start work on buildPadArea function

--- assets/js/songRemixer.js
+++ assets/js/songRemixer.js
@@ -1,24 +1,39 @@
-function buildPadsArea() {
+let stems1 = ['drums', 'percussion', 'bass', 'guitar', 'synths', 'lead guitar', 'fx', 'lead vocal', 'backing']
 
-    const resetPads = document.getElementById('pads-container');
-    resetPads.innerHTML = '';
+let stems2 = ['drums', 'percussion', 'bass', 'guitar']
 
-    function createPads() {
-        const padContainer = document.getElementById('pads-container');
-        const pad = document.createElement('div');
-        pad.classList.add('pad');
 
-        padContainer.appendChild(pad);
-    }
 
-    let stems = ['drums', 'percussion', 'bass', 'guitar', 'synths', 'lead guitar', 'fx', 'lead vocal', 'backing vocal']
+function createPads() {
+    const padContainer = document.getElementById('pads-container');
+    const pad = document.createElement('div');
+    pad.classList.add('pad');
 
-    for (let i = 0; i < stems.length; i++) {
+    padContainer.appendChild(pad);
+}
+
+function buildPadsAreaOne() {
+    let padReset = document.getElementById('pads-container');
+    padReset.innerText = '';
+
+    for (let i = 0; i < stems1.length; i++) {
         createPads();
     };
-
 };
 
-// document.getElementById('song1Selector').addEventListener('click', function () {
-//     buildPadsArea();
-// });
+function buildPadsAreaTwo() {
+    let padReset = document.getElementById('pads-container');
+    padReset.innerText = '';
+
+    for (let i = 0; i < stems2.length; i++) {
+        createPads();
+    };
+};
+
+
+//jQuery Functions
+
+$(document).ready(function () {
+    $('#song1Selector').on('click', buildPadsAreaOne);
+    $('#song2Selector').on('click', buildPadsAreaTwo);
+});
