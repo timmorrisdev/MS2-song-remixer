2875a7d67792d3689cfb833ca1ba73c7bf11fc43
TimMorrisDev <timmorris_16@hotmail.com>
2021-07-23 10:01:56 +0000

    create contact form

---
+++ assets/js/sendContact.js
@@ -0,0 +1,17 @@
+function sendContact(contactForm) {
+    emailjs.send("timMorrisDev", "template_jl3kepn", {
+            "from_name": contactForm.name.value,
+            "from_email": contactForm.emailaddress.value,
+            "comment_content": contactForm.commentContent.value
+        })
+
+        .then(
+            function (response) {
+                console.log("SUCCESS", response);
+            },
+            function (error) {
+                console.log("FAILED", error);
+            }
+        );
+    return false; // To block from loading a new page
+};
