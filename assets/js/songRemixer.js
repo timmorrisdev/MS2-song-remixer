51f8c4a586780b4447fe6ab783b82f162773005e
TimMorrisDev <timmorris_16@hotmail.com>
2021-07-24 15:12:41 +0000

    use portion of template from howlerJS documentaiton and begin editing to suit site purpose

--- assets/js/songRemixer.js
+++ assets/js/songRemixer.js
@@ -1,31 +1,33 @@
 // Cache references to DOM elements.
-const elms = ['pad0', 'pad1', 'pad2', 'pad3', 'pad4', 'pad5', 'pad6', 'pad7', 'pad8'];
+/*const elms = ['pad0', 'pad1', 'pad2', 'pad3', 'pad4', 'pad5', 'pad6', 'pad7', 'pad8'];
 elms.forEach(function (elm) {
     window[elm] = document.getElementById(elm);
 });
+*/
 
-var Sprite = function (options) {
-    var self = this;
+class Sprite {
+    constructor(options) {
+        var self = this;
 
-    self.sounds = [];
+        self.sounds = [];
 
-    // Setup the options to define this sprite display.
-    self._spriteMap = options.spriteMap;
-    self._sprite = options.sprite;
-    self.setupListeners();
+        // Setup the options to define this sprite display.
+        self._spriteMap = options.spriteMap;
+        self._sprite = options.sprite;
+        self.setupListeners();
 
-    // Create our audio sprite definition.
-    self.sound = new Howl({
-        src: options.src,
-        sprite: options.sprite
-    });
+        // Create our audio sprite definition.
+        self.sound = new Howl({
+            src: options.src,
+            sprite: options.sprite
+        });
 
-};
-Sprite.prototype = {
+    }
     /**
      * Setup the listeners for each sprite click area.
      */
-    setupListeners: function () {
+    /*
+    setupListeners() {
         var self = this;
         var keys = Object.keys(self._spriteMap);
 
@@ -34,40 +36,23 @@ Sprite.prototype = {
                 self.play(key);
             }, false);
         });
-    },
-
+    }
+    */
     /**
      * Play a sprite when clicked and track the progress.
      * @param  {String} key Key in the sprite map object.
      */
-    play: function (key) {
+    play(key) {
         var self = this;
         var sprite = self._spriteMap[key];
 
         // Play the sprite sound and capture the ID.
         var id = self.sound.play(sprite);
 
-        // Create a progress element and begin visually tracking it.
-        var elm = document.createElement('div');
-        elm.className = 'progress';
-        elm.id = id;
-        elm.dataset.sprite = sprite;
-        window[key].appendChild(elm);
-        self.sounds.push(elm);
+    }
+}
 
-        // When this sound is finished, remove the progress element.
-        self.sound.once('end', function () {
-            var index = self.sounds.indexOf(elm);
-            if (index >= 0) {
-                self.sounds.splice(index, 1);
-                window[key].removeChild(elm);
-            }
-        }, id);
-    },
-
-};
-
-var sprite = new Sprite({
+const sprite = new Sprite({
     "src": [
         // "assets/songs/aloosh/aloosh.webm",
         "assets/songs/aloosh/aloosh.mp3"
@@ -122,6 +107,12 @@ var sprite = new Sprite({
         pad8: 'vocalLead',
     }
 });
+
+function playAudio() {
+    for (let i = 0; i < spriteMap.length; i++) {
+
+    }
+}
 
 
 
